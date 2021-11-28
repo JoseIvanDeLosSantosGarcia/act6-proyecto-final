8491d9b9d0d3966c727a6d5a047d1f56d794336c
joseivandelossantosgarcia <sagi040108hchnrva2@gmail.com>
2021-11-28 16:08:09 -0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<aside>
+				<div class="widget">
+					<h3>LAS MEJORES OFERTAS.</h3>
+					<ul>
+						<li><a href= "#">OFERTAS</a></li>
+						<li><a href= "#">CUPONES EN MONEDERO</a></li>
+						<li><a href= "#">CARNES Y CEREALES</a></li>
+						<li><a href= "#">FRUTAS Y VERDURAS</a></li>
+						<li><a href="index.php">INICIO</a></li>
+					<li><a href="crud_empleado/empleados.php">Empleados</a></li>
+					<li><a href="crud_cliente/cliente.php">Clientes</a></li>
+					<li><a href="crud_articulo/articulo.php">Articulos</a></li>
+					<li><a href="crud_venta/venta.php">Ventas</a></li>
+				</ul>
+</ul>
+				<div cente><a href "#"><img src=img/ofertass.jpg width="300" height="200" ></a></div>
+</div>
+			</aside>
